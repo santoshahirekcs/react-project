fd92724feb95861492c4e7ca97783e41b0752341
santoshahirekcs <santosh.ahire@kcspl.co.in>
2019-11-12 12:21:00 +0530

    second commit

---
+++ build/precache-manifest.5409d963b84c8ee7211d46e4ffbafc9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d227aea43e20e758d0cc0c97695d42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "953b61094fe0949725a9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e6fdd062c1b92fed175c",
+    "url": "/static/js/2.b829006e.chunk.js"
+  },
+  {
+    "revision": "953b61094fe0949725a9",
+    "url": "/static/js/main.ae351e23.chunk.js"
+  },
+  {
+    "revision": "39d2da9dfcaee98d236f",
+    "url": "/static/js/runtime-main.74d82ba6.js"
+  },
+  {
+    "revision": "9a2c9911e532dc1a04a96c0ea86d8b3b",
+    "url": "/static/media/team.9a2c9911.png"
+  }
+]);
